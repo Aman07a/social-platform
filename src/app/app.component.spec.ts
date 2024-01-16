48ccac353ba2ad95892917e066b5c785d82673ae
Aman07a <aman16.aa17@gmail.com>
2024-01-16 14:10:04 +0100

    Running unit tests using Karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { AppComponent } from './app.component';
 
+// Running unit tests using Karma
+// ng test
 describe('AppComponent', () => {
   it('should have a defined title', () => {
     const component = new AppComponent();
